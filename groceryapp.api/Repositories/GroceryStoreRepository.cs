225d4bc75407ba98a608e37de53a0ac3197ba9ae
Josh <jpantana@clearviewsoftware.net>
2020-01-11 20:00:41 -0600

    real-time db introduced

--- groceryapp.api/Repositories/GroceryStoreRepository.cs
+++ groceryapp.api/Repositories/GroceryStoreRepository.cs
@@ -10,8 +10,8 @@ namespace groceryapp.api.Repositories
 {
     public class GroceryStoreRepository : IGroceryStoreRepository
     {
-        string _connectionString = "Server=localhost;Database=GroceriesDb2;Trusted_Connection=True;";
-
+        // string _connectionString = "Server=localhost;Database=GroceriesDb2;Trusted_Connection=True;";
+        string _connectionString = "Server=tcp:groceryappserver.database.windows.net,1433;Initial Catalog=GroceryDb;Persist Security Info=False;User ID=jpantana;Password=GroceryAppPwd1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
         public IEnumerable<GroceryStore> GetAllGroceryStores()
         {
             using (var db = new SqlConnection(_connectionString))
